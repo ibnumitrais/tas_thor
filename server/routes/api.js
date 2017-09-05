66f32b1c56b5a08f3d137f7e74da93197b64e5ae
Ibnu Prayogi <ibnu.prayogi@mitrais.com>
2017-09-05 08:44:00 +0800

    - initial server - add tedious model - read json file

---
+++ server/routes/api.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+var user = require("../model/user");
+var grade = require("../model/grade");
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+// API for TAS
+router.get('/grades', function(req, res) {
+	grade.getRecords(function(status, data){
+		if(status > 0) {
+			res.writeHead(200, {"Content-Type": "text/json"});
+			res.end(JSON.stringify(data));
+		}
+		else {console.log(data);}
+	}, '');
+});
+
+router.get('/users', function(req, res) {
+
+	user.getRecords(function(status, data){
+		if(status > 0) {
+			res.writeHead(200, {"Content-Type": "text/json"});
+			res.end(JSON.stringify(data));
+		}
+		else {console.log(data);}
+	}, '');
+});
+
+router.post('/auth', function(req, res) {
+	if(req.body.username == req.body.password)
+		res.json({
+			"status" : 'success',
+			"user": {
+				'username' : req.body.username,
+				'email' : req.body.username+'@mitrais.com',
+			}
+		});
+	else
+		res.json({
+			"status" : 'failed',
+			"message": "Username or password is not match."
+		});
+});
+
+router.get('/tes', function(req, res) {
+  var obj;
+  fs.readFile('data/users.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    res.json(obJSON.parse(data));
+  });
+})
+
+module.exports = router;
